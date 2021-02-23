6695422687c38f00a265cb91c0d9bc070cfdd5fd
Zappar <hello@zappar.com>
2021-02-23 09:24:18 +0000

    Example for Zappar React Three Fiber

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { Suspense, useRef } from 'react';
+import { useLoader } from 'react-three-fiber';
+import { TextureLoader } from 'three';
+import {
+ FaceBufferGeometry, FaceTracker, ZapparCamera, ZapparCanvas,
+} from '@zappar/zappar-react-three-fiber';
+
+import faceMapSrc from './assets/faceMeshTemplate.png'
+
+const FaceMeshMaterial = () => {
+    const faceMapTexture = useLoader(TextureLoader, faceMapSrc);
+    return <meshStandardMaterial transparent map={faceMapTexture} />;
+};
+
+function App() {
+    const trackerGroup = useRef()
+    return (
+      <ZapparCanvas>
+        <ZapparCamera userFacing rearCameraMirrorMode="css"/>
+        <FaceTracker ref={trackerGroup}>
+          <Suspense fallback={null}>
+            <mesh>
+              <FaceMeshMaterial />
+              <FaceBufferGeometry trackerGroup={trackerGroup}/>
+            </mesh>
+          </Suspense>
+
+        </FaceTracker>
+        <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+      </ZapparCanvas>
+    );
+}
+
+export default App;
